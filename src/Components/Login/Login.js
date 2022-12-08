1aa1e7bf774270a0115ebd3c5487b24e3a602c52
mgreen1092 <morgan.green0209@gmail.com>
2022-12-08 12:22:12 -0500

    updated usernames

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,9 +6,9 @@ const Login = () => {
     //const [user, setUser] = useState()
     return (
         <div>
-            <h3 className='user'><Link to='/morgan'>Morgan</Link></h3>
-            <h3 className='user'><Linke to='/ana'>Ana</Linke></h3>
-            <h3 className='user'><Link to='/eli'>Eli</Link></h3>
+            <h3 className='user'><Link to='/mgreen1092'>mgreen1092</Link></h3>
+            <h3 className='user'><Link to='/aaixn'>aaixn</Link></h3>
+            <h3 className='user'><Link to='/esaltzman'>esaltzman</Link></h3>
         </div>
     )
 }
