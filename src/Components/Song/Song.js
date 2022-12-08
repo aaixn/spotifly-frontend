68a8ca394735b17e77cfe1689ed2afcf9c2acf9e
mgreen1092 <morgan.green0209@gmail.com>
2022-12-08 14:59:28 -0500

    added params to axios link

--- src/Components/Song/Song.js
+++ src/Components/Song/Song.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import './Song.css'
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Song = () => {
     const [songs, setSongs] = useState
+    let params = useParams()
     const getSongs = () => {
         //I dont think this link is right - i think we need to set a state for the user and interpolate that in to the link??
-        axios.get(`http://localhost:8080/api/users/${username}`).then(response => {
+        axios.get(`http://localhost:8080/api/playlist/${params.id}`).then(response => {
             setSongs(response.data.songs)
         })
     }
