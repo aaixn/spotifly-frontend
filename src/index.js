c7b10be51c041ba4840a27f393dde44c37ce5614
esaltzm <elisaltzman@gmail.com>
2022-12-09 10:43:37 -0500

    add browser router to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement)
 
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
