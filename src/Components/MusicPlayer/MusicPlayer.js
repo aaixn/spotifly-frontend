c54566e455503a263e1571173ce7f5e150733ecd
mgreen1092 <morgan.green0209@gmail.com>
2022-12-09 16:31:37 -0500

    set up music player

---
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+const MusicPlayer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default MusicPlayer
