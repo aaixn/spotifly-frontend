b0b49745a379ee26368a60e7ea0def0aaa4c6ee4
esaltzm <elisaltzman@gmail.com>
2022-12-09 12:01:14 -0500

    add routes back in with conditional rendering of navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 function App() {
   const [users, setUsers] = useState()
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
     <div className='login'>
-      <Login users={users} setUsers={setUsers}/>
-      {/* <Nav /> */}
-      {/* <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes> */}
+      {loggedIn ? <Nav /> : <></>}
+      <Routes>
+        <Route path='/' element={<Login users={users} setUsers={setUsers} />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
     </div>
   );
 }
