d4d6948b24a17059e3ad7552f8f92d83337681e1
esaltzm <elisaltzman@gmail.com>
2022-12-09 11:39:29 -0500

    add axios get for user playlists to nav bar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import {IoIosAddCircle} from 'react-icons/io'
-import {GrHomeRounded} from 'react-icons/gr'
+import { IoIosAddCircle } from 'react-icons/io'
+import { GrHomeRounded } from 'react-icons/gr'
+import axios from 'axios'
 import '../Nav/Nav.css'
 
 export default function Nav() {
-  return (
-    <div className='nav'>
-        <Link to='/'><div className='nav-home'>
-            <GrHomeRounded />
-            <p>Home</p>
-        </div></Link>
-        <div className='nav-playlists'>
-            <p>Playlists</p>
-            <IoIosAddCircle className='add-playlist-button' style={{fontSize: '1.3em'}}/>
-        </div>
-        <div className='nav-playlist-list'>List of Playlists</div>
-    </div>
-  )
+	const [playlists, setPlaylists] = useState([])
+	useEffect(() => {
+		const getPlaylists = async (userID) => {
+			const res = await axios.get(`https://spotifly-backend-ga.herokuapp.com/api/users/${userID}`)
+			const gotPlaylists = res.data.playlists
+			setPlaylists(gotPlaylists)
+		}
+		getPlaylists('63935095d2cd174067383ea4')
+	}, [])
+
+	return (
+		<div className='nav'>
+			<Link to='/'><div className='nav-home'>
+				<GrHomeRounded />
+				<p>Home</p>
+			</div></Link>
+			<div className='nav-playlists'>
+				<p>Playlists</p>
+				<IoIosAddCircle className='add-playlist-button' style={{ fontSize: '1.3em' }} />
+			</div>
+			<div className='nav-playlist-list'>
+				{playlists.map(playlist => <div className='playlist-item'>{playlist.name}</div>)}
+			</div>
+		</div>
+	)
 }
