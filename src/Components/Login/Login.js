ff1538b098c800c64fa38e2729a8f0f3474d06cd
mgreen1092 <morgan.green0209@gmail.com>
2022-12-09 12:43:51 -0500

    fixed routes to login page and playlistList

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from '../Home/Home'
+import axios from 'axios'
 //import { useState } from 'react'
 
-const Login = () => {
+const Login = ({user, setUser}) => {
     //const [user, setUser] = useState()
+    const handleClick = async (event) => {
+        console.log(event.target.name)
+       const response = await axios.get(`https://spotifly-backend-ga.herokuapp.com/api/users/${event.target.name}`)
+       console.log(response.data)
+        setUser(response.data)
+        console.log(user)
+
+        //setUser(event.target.id)
+    }
     return (
-        <div class='user-selection'>
-            <h3 className='user-morgan'><Link to='/63935095d2cd174067383ea6'>Morgan</Link></h3>
-            <h3 className='user-ana'><Link to='/63935095d2cd174067383ea5'>Ana</Link></h3>
-            <h3 className='user-eli'><Link to='/63935095d2cd174067383ea4'>Eli</Link></h3>
+        <div className='user-selection'>
+            <h3 id='63935095d2cd174067383ea6' className='user-morgan'><Link to='/63935095d2cd174067383ea6/home'>Morgan</Link></h3>
+            <h3 id='63935095d2cd174067383ea5' className='user-ana'><Link to='/63935095d2cd174067383ea5/home'>Ana</Link></h3>
+            <Link to='/63935095d2cd174067383ea4/home' onClick={(event)=>{handleClick(event)}} name ='63935095d2cd174067383ea4' className='user-eli'>Eli</Link>
         </div>
     )
 }
