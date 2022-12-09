229396a3be8badbbe3059bcd02928f99ed35d1e3
mgreen1092 <morgan.green0209@gmail.com>
2022-12-09 11:46:52 -0500

    login page to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import {useState} from 'react'
+
 
 function App() {
+  const [users, setUsers] = useState()
   return (
-    <div className="App">
-      <Login />
+    <div className='login'>
+      <Login users={users} setUsers={setUsers}/>
       {/* <Nav /> */}
       {/* <Routes>
         <Route path='/' element={<Home />} />
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,17 +6,10 @@ import { Link } from 'react-router-dom'
 const Login = () => {
     //const [user, setUser] = useState()
     return (
-        <div>
-            <div class='user-selection'>
-                <h3 className='user-morgan'><Link to='/63935095d2cd174067383ea6'>Morgan</Link></h3>
-            </div>
-            <div class='user-selection'>
-                <h3 className='user-ana'><Link to='/63935095d2cd174067383ea5'>Ana</Link></h3>
-            </div >
-            <div class='user-selection'>
-                <h3 className='user-eli'><Link to='/63935095d2cd174067383ea4'>Eli</Link></h3>
-            </div>
-            
+        <div class='user-selection'>
+            <h3 className='user-morgan'><Link to='/63935095d2cd174067383ea6'>Morgan</Link></h3>
+            <h3 className='user-ana'><Link to='/63935095d2cd174067383ea5'>Ana</Link></h3>
+            <h3 className='user-eli'><Link to='/63935095d2cd174067383ea4'>Eli</Link></h3>
         </div>
     )
 }
