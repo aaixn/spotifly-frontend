2024989f20eced14e2e84fb03f1ae4397c5aad28
mgreen1092 <morgan.green0209@gmail.com>
2022-12-09 11:18:14 -0500

    added login to app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Login />
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
