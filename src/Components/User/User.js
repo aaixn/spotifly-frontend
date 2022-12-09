772203a331548eb6052bc4960f5b0a2ce7962bbe
mgreen1092 <morgan.green0209@gmail.com>
2022-12-08 19:40:36 -0500

    user component layout

---
+++ src/Components/User/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './User.css'
+
+const User = () => {
+    //axios call to playlist ID to have the playlist name appear
+    //user image in the top right corner
+    return (
+        <div>
+
+        </div>
+    )
+}
