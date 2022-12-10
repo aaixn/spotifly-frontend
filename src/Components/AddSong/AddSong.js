7952cc1ba99868f302dc3821ac80d346b589c8f4
aaixn <axnguyen246@gmail.com>
2022-12-10 14:37:06 -0800

    functionality of delete song and start functionality of add song

--- src/Components/AddSong/AddSong.js
+++ src/Components/AddSong/AddSong.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
-export default function AddSong() {
+export default function AddSong({playlist, user}) {
+
+  const [addSong, setAddSong] = useState(
+    {
+      name: '',
+      artist: '',
+      album: '',
+      duration: '',
+      'playlist-select': playlist ? playlist.name : null
+    }
+  )
+
+  const handleChange = (e) => {
+    const addedSong = {...addSong}
+    addedSong[e.target.name] = e.target.value
+    setAddSong(addedSong)
+  }
+
+  const addSongToPlaylist = async () => {
+    try {
+      const targetedPlaylist = user.playlists.map(item => item.name = addSong['playlist-select'])
+      console.log(targetedPlaylist);
+      // targetedPlaylist = await axios.get(`https://spotifly-backend-ga.herokuapp.com/api/playlists/${targetedPlaylist._id}`)
+      // const update = await axios.put(`https://spotifly-backend-ga.herokuapp.com/api/playlists/${id}`, {...playlist, songs: {...playlist.songs, addSong}} )
+      console.log({...playlist, songs: {...playlist.songs, addSong}});
+    } catch (err) {
+      console.log(err);
+  }
+  }
   return (
     <div>
         <form>
             <h1>Add a Song to a Playlist</h1>
-            <input className='name-input' placeholder='Song Title'></input>
-            <input className='artist-input' placeholder='Artist'></input>
-            <input className='album-input' placeholder='Album Title'></input>
-            <input className='duration-input' placeholder='Duration in Seconds'></input>
+            <input className='name-input' placeholder='Song Title' name='name' value={addSong.name} onChange={handleChange}></input>
+            <input className='artist-input' placeholder='Artist' name='artist' value={addSong.artist} onChange={handleChange}></input>
+            <input className='album-input' placeholder='Album Title' name='album' value={addSong.album} onChange={handleChange}></input>
+            <input className='duration-input' placeholder='Duration in Seconds' name='duration' value={addSong.duration} onChange={handleChange}></input>
             <label for='playlist-select'></label>
-            <select name='playlist-select'>
-                <option value='playlist1'>playlist1</option>
-                <option value='playlist2'>playlist2</option>
-                <option value='playlist3'>playlist3</option>
+            <select name='playlist-select' onChange={handleChange}>
+                {user ? user.playlists.map((item) => {
+                  return (
+                    item.name === playlist.name ? <option value={item.name} selected>{item.name}</option> : <option value={item.name}>{item.name}</option>
+                  )
+                }) : null}
             </select>
-            <button>Add To Playlist</button>
+            <button onClick={addSongToPlaylist}>Add To Playlist</button>
         </form>
     </div>
   )
