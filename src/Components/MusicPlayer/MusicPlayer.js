7fbe3bbb1c7b5d2656350a2db8628cca0fcac950
mgreen1092 <morgan.green0209@gmail.com>
2022-12-11 14:32:02 -0500

    removed commented code

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -1,55 +1,9 @@
 import React from "react";
 //import axios from "axios";
-import {useState, useEffect} from 'react'
-import {ReactPlayer} from 'react-player'
+import {useEffect} from 'react'
 import jsonp from 'jsonp';
 
 const MusicPlayer = () => {
-    // const [musicLinks] = useState([
-        // "https://soundcloud.com/noahkahan/false-confidence?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-    //   ]);
-    // const [data, setData] = useState([{link: new Audio ("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/499655217&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"), play: false}]);
-    //   useEffect(() => {
-    //     const musicData = musicLinks.map((sound) => {
-    //       return { audio: new Audio(sound), play: false };
-    //     });
-    
-    //     setData(musicData);
-    //   }, []);
-      //setData ( {audio: new Audio(musicLinks[0]), play: false } )
-    //   const playSound = (index) => {
-    //     setData((arr) =>
-    //       arr.map((sound, i) => {
-    //         if (i === index) {
-    //           sound.audio.play();
-    //           return { ...sound, play: true };
-    //         }
-    //         sound.audio.pause();
-    //         return { ...sound, play: false };
-    //       })
-    //     );
-    //   };
-    // const playSound = (event) => {
-    //     event.preventDefault()
-    //     console.log(data[0].link)
-    //     data[0].link.play()
-    // }
-    //   useEffect(() => {
-    //     console.log(data);
-    //   }, [data]);
-    
-      const stopSound = (index) => {
-        // setData((arr) =>
-        //   arr.map((sound, i) => {
-        //     if (i === index) {
-        //       sound.audio.pause();
-        //       return { ...sound, play: false };
-        //     }
-        //     return { ...sound, play: false };
-        //   })
-        // );
-      };
-    
       useEffect (() => {
         const soundcloudUrl = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/499655217&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true';
         
@@ -65,19 +19,7 @@ const MusicPlayer = () => {
         
       return (
         <div className="App">
-          {/* {data.map((sound, i) => {
-            return (
-              <>
-                {sound.play ? (
-                  <button onClick={() => stopSound(i)}>pause</button>
-                ) : (
-                  <button onClick={() => playSound(i)}>play</button>
-                )}
-              </>
-            );
-          })} */}
           <iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/499655217&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-                  {/* <button onClick={(event) => playSound(event)}>play</button> */}
           <h2>Start editing to see some magic happen!</h2>
         </div>
       );
