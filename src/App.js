a19f5a913ec94c4d58a2898bdae558a8ef169b00
aaixn <axnguyen246@gmail.com>
2022-12-12 09:20:18 -0800

    basic login page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import { useEffect, useState } from 'react'
 import Playlist from './Components/Playlist/Playlist';
-
+import Form from './Components/Form/Form';
 
 function App() {
   const [user, setUser] = useState('')
   const [playlist, setPlaylist] = useState({})
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
   useEffect(() => {
     window.localStorage.setItem('user', JSON.stringify(user))
   }, [user])
@@ -17,9 +21,11 @@ function App() {
     <div className='login'>
       {user ? <Nav user={user} setUser={setUser} /> : <></>}
       <Routes>
-        <Route path='/' element={<Login user={user} setUser={setUser} />} />
+        <Route path='/login' element={<Form title='Log In'/>} />
+        <Route path='/register' element={<Form title='Register'/>}/>
         <Route path='/home' element={<Home setUser={setUser} />} />
         <Route path='/playlist/:id' element={<Playlist user={user} setUser={setUser} playlist={playlist} setPlaylist={setPlaylist} />} />
+        
       </Routes>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTLA9PKEdHM4ppHfJCkL2m6HsWJgnEqmM",
+    authDomain: "spotifly-2ef38.firebaseapp.com",
+    projectId: "spotifly-2ef38",
+    storageBucket: "spotifly-2ef38.appspot.com",
+    messagingSenderId: "802715144341",
+    appId: "1:802715144341:web:e053dc5060af3f26614b9c"
+  };
+
+export const app = initializeApp(firebaseConfig);
+
