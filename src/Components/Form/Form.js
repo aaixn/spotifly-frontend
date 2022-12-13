2bd67f1921fdc73d1c1316ecfe6f5fa054d68b75
esaltzm <elisaltzman@gmail.com>
2022-12-12 19:46:33 -0500

    add sign up button redirecting to register page

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '../Button/Button';
 
-export default function Form({title, setEmail, setPassword, handleAction}) {
-
+export default function Form({ title, setEmail, setPassword, handleAction }) {
+    const navigate = useNavigate()
     return (
         <div>
             <div className="heading-container">
@@ -12,7 +13,6 @@ export default function Form({title, setEmail, setPassword, handleAction}) {
                     {title} Form
                 </h3>
             </div>
-
             <Box
                 component="form"
                 sx={{
@@ -21,14 +21,12 @@ export default function Form({title, setEmail, setPassword, handleAction}) {
                 noValidate
                 autoComplete="off"
             >
-                <TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)}/>
-                <TextField id="password" label="Enter the Password" variant="outlined" onChange={(e) => setPassword(e.target.value)}/>
-                {/* require password to be at least 6 chara 
-                    make
-                */}
+                <TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
+                <TextField id="password" label="Enter the Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
             </Box>
 
-            <Button title={title} handleAction={handleAction}/>
+            <Button title={title} handleAction={handleAction} />
+            {title === 'Log In' && <Button title='Sign Up' handleAction={() => { navigate('/register') }} />}
         </div>
     );
 }
