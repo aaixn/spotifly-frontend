2d24b603c2f184ab9c26d6faa4d623bc8cd04b92
esaltzm <elisaltzman@gmail.com>
2022-12-13 10:35:13 -0500

    reduce code in musicplayer

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -1,36 +1,11 @@
-import React from "react";
-//import axios from "axios";
-import {useEffect} from 'react'
-import jsonp from 'jsonp';
-import axios from "axios";
+import React from "react"
 
-const MusicPlayer = ({playingNow}) => {
-    
-    
-    //   useEffect ( () => {
-    //     const url = 'https://soundcloud.com/noahkahan/growing-sideways'
-        // const getTrackID = async () => {
-        //     const response = await axios.get(`https://w.soundcloud.com/player/?url=${url}`)
-        //     let trackID = response.url
-        //     console.log(trackID)
-           
-        // }
-        // console.log('useEffect')
-        // jsonp(`https://w.soundcloud.com/player/?url=${url}`, {}, (err, data) => {
-        //     if(err) {
-        //         console.log(err)
-        //     } else (
-        //         console.log(data.url, 'URL')
-        //     )
-        // })
-        // getTrackID()
-        // }, [])
-      return (
-        <div className="App">
-          <iframe src={playingNow}></iframe>
-          <h2>Start editing to see some magic happen!</h2>
-        </div>
-      );
-    }
-    
+const MusicPlayer = ({ playingNow }) => {
+  return (
+    <div className="music-player">
+      <iframe src={playingNow}></iframe>
+    </div>
+  );
+}
+
 export default MusicPlayer
