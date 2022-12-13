ec32ad37ee3edb3b4423ed9e677e519929f4f7af
esaltzm <elisaltzman@gmail.com>
2022-12-13 17:28:46 -0500

    add default images to home screen playlist grid

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,25 +2,30 @@ import React, { useEffect } from 'react'
 import '../Home/Home.css'
 import { useNavigate } from 'react-router-dom'
 
-export default function Home({ setUser }) {
+export default function Home({ user, setUser }) {
 
-  let navigate = useNavigate();
   useEffect(() => {
-    let authToken = sessionStorage.getItem('Auth Token')
-    if (authToken) {
-      navigate('/home')
-    }
+    let refreshUser
+    if (localStorage.getItem('user') !== 'undefined' || undefined) refreshUser = JSON.parse(localStorage.getItem('user'))
+    refreshUser && setUser(refreshUser)
   }, [])
 
   return (
     <div className='home'>
       <div className='announcement'></div>
-      <h2>Hello user</h2>
-      <div className='discover'>
-        <h3>Discover</h3>
-      </div>
-      <div className='genres'>
-        <h3>Listen by Genre</h3>
+      <h2>Hello <span style={{ color: '#1BD760' }}>{user && user.email.split('@')[0]}</span>!</h2>
+      <div className='playlists-section'>
+        <h3>Your Playlists</h3>
+        {user && <div className='playlist-grid'>
+          {user.playlists.map(playlist => {
+            return (
+              <div className='playlist-grid-item'>
+                <div>{playlist.name}</div>
+                <img src={playlist.image} alt='playlist cover art' style={{ width: '200px' }} />
+              </div>
+            )
+          })}
+        </div>}
       </div>
     </div>
   )
