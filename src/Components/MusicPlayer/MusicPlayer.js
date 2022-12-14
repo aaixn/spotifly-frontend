33f35088f37299d0b6400d07a60f2961739c71c1
esaltzm <elisaltzman@gmail.com>
2022-12-14 11:53:54 -0500

    conditional rendering of music player

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -4,18 +4,17 @@ import './MusicPlayer.css'
 const MusicPlayer = ({ playingNow }) => {
   return (
     <div className="music-player">
-      <iframe
+      {playingNow && <iframe
         margin="0"
-        width="100%" 
-        height="166" 
-        scrolling="no" 
-        frameBorder="no" 
-        allow="autoplay" 
+        width="100%"
+        height="166"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
         src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${playingNow}&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=false`}>
-      </iframe>
-
+      </iframe>}
     </div>
-  );
+  )
 }
 
 export default MusicPlayer
